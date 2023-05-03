8364377daf009ba406fd6c86448c788b58f70a5b
c-kiplimo <kiplimocollins855@gmail.com>
2023-05-03 20:48:39 +0300

    fix

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, duplicate_ignore
 
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:wallet_ui/util/my_card.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,6 +12,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //pageController
+  final _controller = PageController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,42 +52,50 @@ class _HomePageState extends State<HomePage> {
               ],
             ),
           ),
-          SizedBox(height: 25,),
+          SizedBox(
+            height: 25,
+          ),
           Container(
             height: 200,
             child: PageView(
-              scrollDirection: Axis.horizontal
-              ,children: [
-              MyCard(
-                balance: 5250.20,
-                cardNumber: 12345678,
-                expirymonth: 10,
-                expiryYear: 24,
-                color: Colors.deepPurple[400],
-              ),
-               MyCard(
-                balance: 3250.70,
-                cardNumber: 12345678,
-                expirymonth: 11,
-                expiryYear: 26,
-                color: Colors.blue[400],
-              ),
-               MyCard(
-                balance: 5230.60,
-                cardNumber: 12345678,
-                expirymonth: 12,
-                expiryYear: 25,
-                color: Colors.geen[400],
-              ),
-            ],),
+              scrollDirection: Axis.horizontal,
+              controller: _controller,
+              children: [
+                MyCard(
+                  balance: 5250.20,
+                  cardNumber: 12345678,
+                  expirymonth: 10,
+                  expiryYear: 24,
+                  color: Colors.deepPurple[300],
+                ),
+                MyCard(
+                  balance: 3250.70,
+                  cardNumber: 12345678,
+                  expirymonth: 11,
+                  expiryYear: 26,
+                  color: Colors.blue[300],
+                ),
+                MyCard(
+                  balance: 5230.60,
+                  cardNumber: 12345678,
+                  expirymonth: 12,
+                  expiryYear: 25,
+                  color: Colors.green[300],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 25,),
+          SmoothPageIndicator(controller: _controller, count: 3,
+          effect: ExpandingDotsEffect(
+            activeDotColor: Colors.grey.shade700,
+          ),),
+           SizedBox(height: 25,),
+          Row(
+            children: [
+              Container(child: Center(child: Image.asset('lib/') ),)
+            ],
           )
-         
-
-          //cards
-
-          //3 buttons
-
-          //column -> stats + transaction
         ]),
       ),
     );
